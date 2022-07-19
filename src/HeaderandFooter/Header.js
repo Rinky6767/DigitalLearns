f54367aaffaf526cc1f3b9f60a69e46b3858a931
Rinky <rinkymaurya06767@gmail.com>
2022-07-19 12:29:23 +0530

    side nav

--- src/HeaderandFooter/Header.js
+++ src/HeaderandFooter/Header.js
@@ -4,12 +4,22 @@ import { FaListUl, FaTimes } from "react-icons/fa";
 import { Anchor } from 'antd';
 import React, { useState } from 'react';
 const { Link } = Anchor;
+
 function Header() {
     let [dply, setDply] = useState("hide");
     let [name, setName] = useState("show");
+    let [list, setlist] = useState("show");
+    window.addEventListener("scroll", () => {
+        if (dply ===w23 "show") {
+            setDply("hide");
+            setName("show");
+            setlist("show");
+        }
+    })
     function clickHandler() {
         setDply("hide");
         setName("show");
+        setlist("show");
     }
     return (<div id="headwrapper">
         <h2 id="nameheader" className={name}>DigitalLearns <FcReadingEbook id="book" /></h2>
@@ -28,18 +38,22 @@ function Header() {
                     <FaTimes id="cross" style={{ marginLeft: "15px", cursor: "pointer" }} onClick={() => {
                         setDply("hide");
                         setName("show");
+                        setlist("show");
                     }} />
-                    <Link href="#couserselwrapper" title="Courses" className='links'/>
-                    <Link href="#popwrap" title="Popular Courses" className='links' />
-                    <Link href="#newcourseswrapper" title="New Courses" className='links' />
-                    <Link href="#formwrapper" title="Contact us" className='links' />
+                    <div id="navlinks">
+                        <Link href="#couserselwrapper" title="Courses" className='links' />
+                        <Link href="#popwrap" title="Popular Courses" className='links' />
+                        <Link href="#newcourseswrapper" title="New Courses" className='links' />
+                        <Link href="#formwrapper" title="Contact us" className='links' />
+                    </div>
                 </div>
             </Anchor>
         </div>
-        <FaListUl id="menus" onClick={() => {
+        <FaListUl id="menus" className={list} onClick={() => {
             if (dply === "hide") {
                 setDply("show");
                 setName("hide");
+                setlist("hide");
             } else {
                 if (dply === "show") {
                     setDply("hide");
